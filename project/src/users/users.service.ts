6ad19186342ef616d46e8b0f450f4d98c4776e28
lyklecharova <lklecharova9@gmail.com>
2024-07-27 17:36:09 +0300

    create  hardcode user list
    create CRUD functionalities

--- project/src/users/users.service.ts
+++ project/src/users/users.service.ts
@@ -1,4 +1,76 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users = [
+    {
+      id: 1,
+      name: 'Pesho Petrov',
+      email: 'pesho@gmail.com',
+      role: 'ADMIN',
+    },
+    {
+      id: 2,
+      name: 'Ivan Ivanov',
+      email: 'ivan.i@gmail.com',
+      role: 'INTERNS',
+    },
+    {
+      id: 3,
+      name: 'Asya Dimitrova',
+      email: 'asya.i@gmail.com',
+      role: 'ENGINEER',
+    },
+  ];
+
+  findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    if (role) {
+      return this.users.filter((user) => user.role === role);
+    }
+    return this.users;
+  }
+
+  findOne(id: number) {
+    const user = this.users.filter((user) => user.id === id);
+    return user;
+  }
+
+  create(user: {
+    name: string;
+    email: string;
+    role: 'INTERN' | 'ENGINEER' | 'ADMIN';
+  }) {
+    const usersByHighestId = [...this.users].sort((a, b) => (b.id - a.id));
+    const newUser = {
+      id: usersByHighestId[0].id + 1,
+      ...user,
+    };
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  update(
+    id: number,
+    updatedUser: {
+      name?: string;
+      email?: string;
+      role?: 'INTERN' | 'ENGINEER' | 'ADMIN';
+    },
+  ) {
+    this.users = this.users.map((user) => {
+      if (user.id === id) {
+        return { ...user, ...updatedUser };
+      }
+      return user;
+    });
+
+    return this.findOne(id);
+  }
+
+  delete(id: number) {
+    const removedUser = this.findOne(id);
+    this.users = this.users.filter((user) => user.id !== id);
+
+    return removedUser;
+  }
+}
